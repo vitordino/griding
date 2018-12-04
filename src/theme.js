ccc16b472c66f36dbb0bed2e82b4dc433ba238b5
Vitor Dino <vitor@kunstdesign.com.br>
2018-12-04 03:27:47 -0200

    :fire: remove unused theme file
    default theme is now on Provider.js

--- src/theme.js
+++
@@ -1,8 +0,0 @@
-export const columns = 12
-export const breakpoints = {
-	xs: { width:    0, gutter: 16 },
-	sm: { width:  480, gutter: 24 },
-	md: { width:  768, gutter: 32 },
-	lg: { width:  992, gutter: 32 },
-	xg: { width: 1280, gutter: 32 },
-}
