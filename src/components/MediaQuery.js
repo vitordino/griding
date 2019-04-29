5a8e468f52883c233f673d32d8bc5ff691987452
Vitor Dino <vitor@kunstdesign.com.br>
2019-04-29 16:17:14 -0300

    :sparkles: add MediaQuery component

---
+++ src/components/MediaQuery.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {withTheme} from 'styled-components';
+import windowSize from 'react-window-size'
+import {parse} from '../utils/dimensions'
+
+
+const getVisibility = ({theme, above, below, windowWidth}) => {
+	const breakpoints = theme.griding.breakpoints
+	const {width: aboveWidth} = breakpoints[above] || {width: above || 0}
+	const {width: belowWidth} = breakpoints[below] || {width: below || 0}
+	return windowWidth >= parse(aboveWidth) && windowWidth <= parse(belowWidth)
+}
+
+const MediaQuery = props => {
+	const visible = getVisibility(props)
+	const children = props.children || props.render
+	if(typeof children === 'function') return children(visible)
+	return visible ? (children || null) : null
+}
+
+MediaQuery.defaultProps = {above: 0, below: Infinity}
+
+export default windowSize(withTheme(MediaQuery))
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as Provider } from './components/Provider'
 export { default as Row } from './components/Row'
 export { default as Cell } from './components/Cell'
+export { default as MediaQuery } from './components/MediaQuery'
 export { default as above } from './utils/above'
