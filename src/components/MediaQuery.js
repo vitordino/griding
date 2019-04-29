4637ffedbfa3c049035040beedbc82d4447c07ca
Vitor Dino <vitor@kunstdesign.com.br>
2019-04-29 16:26:40 -0300

    :bug: MediaQuery: fix conflicting size condition

--- src/components/MediaQuery.js
+++ src/components/MediaQuery.js
@@ -8,7 +8,7 @@ const getVisibility = ({theme, above, below, windowWidth}) => {
 	const breakpoints = theme.griding.breakpoints
 	const {width: aboveWidth} = breakpoints[above] || {width: above || 0}
 	const {width: belowWidth} = breakpoints[below] || {width: below || 0}
-	return windowWidth >= parse(aboveWidth) && windowWidth <= parse(belowWidth)
+	return windowWidth >= parse(aboveWidth) && windowWidth < parse(belowWidth)
 }
 
 const MediaQuery = props => {
