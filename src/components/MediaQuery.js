277c9119d0a7cc57e4162129f8a68d9629831f1b
Vitor Dino <vitor@kunstdesign.com.br>
2019-05-02 11:04:40 -0300

    :recycle: MediaQuery: hook based refactor

--- src/components/MediaQuery.js
+++ src/components/MediaQuery.js
@@ -1,18 +1,10 @@
 import React from 'react'
 import {withTheme} from 'styled-components';
-import windowSize from 'react-window-size'
+import useMediaQuery from '../hooks/useMediaQuery'
 import {parse} from '../utils/dimensions'
 
-
-const getVisibility = ({theme, above, below, windowWidth}) => {
-	const breakpoints = theme.griding.breakpoints
-	const {width: aboveWidth} = breakpoints[above] || {width: above || 0}
-	const {width: belowWidth} = breakpoints[below] || {width: below || 0}
-	return windowWidth >= parse(aboveWidth) && windowWidth < parse(belowWidth)
-}
-
 const MediaQuery = props => {
-	const visible = getVisibility(props)
+	const visible = useMediaQuery(props)
 	const children = props.children || props.render
 	if(typeof children === 'function') return children(visible)
 	return visible ? (children || null) : null
@@ -20,4 +12,4 @@ const MediaQuery = props => {
 
 MediaQuery.defaultProps = {above: 0, below: Infinity}
 
-export default windowSize(withTheme(MediaQuery))
+export default withTheme(MediaQuery)
