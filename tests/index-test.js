66919ba4cae1a4321d0130f20bfb3639f78142c7
Vitor Dino <vitor@kunstdesign.com.br>
2019-05-11 17:24:14 -0300

    :white_check_mark: update render/coverage test

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,8 +1,58 @@
 import expect from 'expect'
 import React from 'react'
+import styled from 'styled-components'
 import {render, unmountComponentAtNode} from 'react-dom'
 
-import {Provider, Row, Cell} from '../src/'
+import {
+	Provider,
+	Row,
+	Cell,
+	MediaQuery,
+	useTheme,
+	useWindowSize,
+	useMediaQuery,
+	useBreakpoints,
+	useCurrentBreakpoint,
+	above,
+} from '../src/'
+
+const StyledAbove = styled.div`
+	display: none;
+	${above('md')`
+		display: block;
+	`}
+`
+
+const Test = () => {
+	const theme = useTheme()
+	const windowSize = useWindowSize()
+	const mediaQuery = useMediaQuery({above: 'md'})
+	const breakpoints = useBreakpoints()
+	const currentBreakpoint = useCurrentBreakpoint()
+	return (
+		<Row>
+			<Cell xs={12} sm={6} md={4} lg={3} xg={2}>
+				<pre>columns: {theme.griding.columns}</pre>
+				<pre>window size: {JSON.stringify(windowSize)}</pre>
+				<pre>window size: {JSON.stringify(windowSize)}</pre>
+				<pre>{mediaQuery ? 'above' : 'below'}</pre>
+				<pre>visible breakpoints {JSON.stringify(breakpoints)}</pre>
+				<pre>current breakpoint {JSON.stringify(currentBreakpoint)}</pre>
+				<StyledAbove>styled above</StyledAbove>
+			</Cell>
+			<MediaQuery above='md'>
+				<Cell xs={12} sm={6} md={4} lg={3} xg={2}>
+					lorem ipsum above
+				</Cell>
+			</MediaQuery>
+			<MediaQuery below='md'>
+				<Cell xs={12} sm={6} md={4} lg={3} xg={2}>
+					lorem ipsum below
+				</Cell>
+			</MediaQuery>
+		</Row>
+	)
+}
 
 describe('Griding', () => {
 	let node
@@ -17,13 +67,7 @@ describe('Griding', () => {
 
 	it('renders without breaking', () => {
 		render(
-			<Provider>
-				<Row>
-					<Cell xs={12} sm={6} md={4} lg={3} xg={2}>
-						lorem ipsum
-					</Cell>
-				</Row>
-			</Provider>
+			<Provider><Test/></Provider>
 			, node, () => {
 			expect(node.innerHTML).toContain('lorem ipsum')
 		})
