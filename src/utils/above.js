496f24061113d4245408a6a4879f95761f2636d6
Vitor Dino <vitor@kunstdesign.com.br>
2019-06-28 01:55:05 -0300

    refactor(above): use above fn to generate utility fn

--- src/utils/above.js
+++ src/utils/above.js
@@ -1,23 +1,19 @@
 import {css} from 'styled-components'
 import {stringify, parse} from './dimensions'
 
-// Iterate through the breakpoints and create a above template
-export const utility = breakpoints => (
-	Object.keys(breakpoints).reduce((acc, label) => {
-		acc[label] = (...args) => css`
-			@media (min-width: ${stringify(parse(breakpoints[label].width))}) {
-				${css(...args)}
-			}
-		`
-		return acc
-	}, {})
-)
-
 // Use a string to generate media query based on breakpoints keys
 export const above = (label) => (...args) => ({theme}) => css`
 	@media (min-width: ${stringify(parse(theme.griding.breakpoints[label].width))}) {
 		${css(...args)}
 	}
 `
+
+// Iterate through the breakpoints and create a above template
+export const utility = breakpoints => (
+	Object.keys(breakpoints).reduce((acc, label) => {
+		acc[label] = (...args) => above(label)([css(...args)])
+		return acc
+	}, {})
+)
 
 export default above
