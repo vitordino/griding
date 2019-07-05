b1e04c7b37301795b90129a26d5ded860a57b9ba
Leonardo Dino <leonardo@kunstdesign.com.br>
2019-07-05 10:58:01 -0300

    :art: reduce code duplication
    :fire: remove example/utils folder
    :lipstick: fix editor style (remove extra paddings)
    :lipstick: add syntax highlight to import statement

--- example/components/Editor.tsx
+++ example/components/Editor.tsx
@@ -1,90 +1,63 @@
 import React from 'react'
 import styled from 'styled-components'
 import { LiveEditor } from 'react-live'
-import dots from '../utils/dots'
-
-const Tab = styled.div`
-	padding-bottom: 1rem;
-	display: block;
-	&:before,
-	& span,
-	&:after {
-		content: '';
-		height: 0.75rem;
-		width: 0.75rem;
-		border-radius: 100%;
-		margin-right: 0.5rem;
-		display: inline-block;
-		border: 1px solid #333;
-	}
-`
-
-const Wrapper = styled.div`
-	border: 2px dashed #333;
-	background: black;
-	box-shadow: 0 1.25rem 4rem rgba(0, 0, 0, 0.55);
-	border-radius: 0.25rem;
-	padding: 1.25rem;
-	max-width: 100%;
-	font-size: 0.875rem;
-	color: #333;
-	position: sticky;
-	top: 6.25rem;
-	margin-bottom: 2rem;
-	${dots}
-	&:focus-within, &:hover, &:active {
-		border-color: white;
-		${Tab}:before {
-			background-color: #f85955;
-			border: 1px solid #d74246;
-		}
-		${Tab} span {
-			background-color: #fbbe3f;
-			border: 1px solid #d7a32d;
-		}
-		${Tab}:after {
-			background-color: #45cc4b;
-			border: 1px solid #40ad36;
-		}
-	}
-`
+import Window from './Window'
 
 const StyledLiveEditor = styled(LiveEditor)`
 	background: none;
 	overflow: scroll;
 	tab-size: 2;
-	& * {
+
+	& > textarea,
+	& > pre {
 		outline: none;
+		padding: 0 !important;
+	}
+	& > pre {
+		user-select: none !important;
 	}
 `
 
-const Import = () => (
-	<pre
-		className='prism-code'
-		style={{ background: 'none', overflow: 'scroll' }}
-	>
-		<span className='token keyword'>import </span>
-		<span className='token punctuation'>{`{ `}</span>
-		<span>Provider</span>
-		<span className='token punctuation'>{`, `}</span>
-		<span>Row</span>
-		<span className='token punctuation'>{`, `}</span>
-		<span>Cell</span>
-		<span className='token punctuation'>{` }`}</span>
-		<span className='token keyword'> from </span>
-		<span className='token string'>'griding'</span>
+const ImportLine = (props: React.ComponentProps<'pre'>) => (
+	<pre {...props}>
+		<span className='keyword'>import</span>
+		<span className='plain'> </span>
+		<span className='punctuation'>{'{'}</span>
+		<span className='plain'> Provider</span>
+		<span className='punctuation'>,</span>
+		<span className='plain'> Row</span>
+		<span className='punctuation'>,</span>
+		<span className='plain'> Cell </span>
+		<span className='punctuation'>{'}'}</span>
+		<span className='plain'> </span>
+		<span className='keyword'>from</span>
+		<span className='plain'> </span>
+		<span className='string'>'griding'</span>
+		{'\n'}
 	</pre>
 )
 
+const StyledImportLine = styled(ImportLine)`
+	overflow: hidden;
+	white-space: pre-wrap;
+	color: rgb(197, 200, 198);
+	.punctuation {
+		opacity: 0.7;
+	}
+	.keyword {
+		color: rgb(209, 148, 158);
+	}
+	.string {
+		color: rgb(245, 184, 61);
+	}
+`
+
 type EditorProps = React.ComponentProps<typeof StyledLiveEditor>
 const Editor = (props: EditorProps) => (
-	<Wrapper>
-		<Tab>
-			<span />
-		</Tab>
-		<Import />
+	<Window background='#000' inactive='#333' active='#fff'>
+		<StyledImportLine />
 		<StyledLiveEditor {...props} />
-	</Wrapper>
+	</Window>
 )
 
 export default Editor
